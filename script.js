3a2098872d9a7674a0d4a8325c0fb3b42da43a63
na-br-wo <nathan.b.wood@gmail.com>
2023-04-18 04:37:51 -0600

    layed out header and main page filler content

---
+++ script.js
@@ -0,0 +1,34 @@
+// script handling the opening/closing of the side bar
+jQuery(function ($) {
+  var $bodyEl = $('body'),
+    $sidedrawerEl = $('#sidedrawer');
+
+
+  function showSidedrawer() {
+    // show overlay
+    var options = {
+      onclose: function () {
+        $sidedrawerEl
+          .removeClass('active')
+          .appendTo(document.body);
+      }
+    };
+
+    var $overlayEl = $(mui.overlay('on', options));
+
+    // show element
+    $sidedrawerEl.appendTo($overlayEl);
+    setTimeout(function () {
+      $sidedrawerEl.addClass('active');
+    }, 20);
+  }
+
+
+  function hideSidedrawer() {
+    $bodyEl.toggleClass('hide-sidedrawer');
+  }
+
+
+  $('.js-show-sidedrawer').on('click', showSidedrawer);
+  $('.js-hide-sidedrawer').on('click', hideSidedrawer);
+});
