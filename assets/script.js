ba5d0a1eb913f2518a65061144dfb07ac1bcc429
na-br-wo <nathan.b.wood@gmail.com>
2023-04-25 22:28:00 -0600

    user search is added to localStorage

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,25 @@ const API_KEY = '93b48d84c3a1a5f16fc9a1e0a72c9875'
 const forecastURL = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
 
 let searchBtn = document.getElementById('searchBtn')
+let cityList = []
+
 
 searchBtn.addEventListener('click', citySearch)
 
 function citySearch() {
+  // array of city search list that will be added to localstorage
+
   const userSearch = document.getElementById('searchbar')
+
+  // displaying cityname in the body of the webpage
   const cityName = document.getElementById('city-name')
   cityName.innerHTML = userSearch.value
+
+  cityList.push(userSearch.value)
+
+  // setting the cityList array to localStorage
+  localStorage.setItem("cityList", JSON.stringify(cityList));
+
 }
 
 
